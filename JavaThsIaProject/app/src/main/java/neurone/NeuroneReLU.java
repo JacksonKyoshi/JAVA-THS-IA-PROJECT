5c86cbc8f2f9de5cbeb13d66b184afbb75da1162
Maxou <Jackson.Kyoshi@gmail.com>
2024-06-07 16:52:41 +0200

    refactor: change root

--- JavaThsIaProject/app/src/main/java/neurone/NeuroneReLU.java
+++
@@ -1,9 +0,0 @@
-package neurone;
-public class NeuroneReLU extends Neurone
-{
-    // Fonction d'activation d'un neurone (peut facilement être modifiée par héritage)
-    protected float activation(final float valeur) {return valeur >= 0 ? valeur : 0.f;}
-
-    // Constructeur
-    public NeuroneReLU(final int nbEntrees) {super(nbEntrees);}
-}
--- JavaThsIaProject/app/src/main/java/neurone/NeuroneSigmoide.java
+++
@@ -1,9 +0,0 @@
-package neurone;
-public class NeuroneSigmoide extends Neurone
-{
-    // Fonction d'activation d'un neurone (peut facilement être modifiée par héritage)
-    protected float activation(final float valeur) {return (float) (1/(1+Math.exp(-valeur)));}
-
-    // Constructeur
-    public NeuroneSigmoide(final int nbEntrees) {super(nbEntrees);}
-}
